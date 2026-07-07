a147ac3fddc8fa0f38fea1b49887241e5081b0ab
perrymaro2010-png <perrymaro2010@gmail.com>
2026-07-07 21:33:14 +0300

    further details in castError handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,16 @@
 const AppError = require('../utils/AppError');
 
 const errorHandler = (err, req, res, next) => {
-    if(err.name == 'CastError' && err.kind === "ObjectId") err = new AppError('Invalid ObjectId', 400);
+    if(err.name == 'CastError')
+        if(err.kind === 'ObjectId'){
+            err = new AppError('Invalid ObjectId', 400);
+        } else if (err.kind === 'Number'){
+            err = new AppError('Must be a Number', 400);
+        } else if (err.kind === 'String'){
+            err = new AppError('Must be a String', 400);
+        } else {
+            err = new AppError('Invalid Data Type', 400);
+        }
     if(err.name == 'ValidationError'){
         const message = Object.values(err.errors)
             .map(e => e.message)
