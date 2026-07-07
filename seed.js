473447f17714de61ec9348e770f07626dc26adcb
perrymaro2010-png <perrymaro2010@gmail.com>
2026-07-07 22:15:09 +0300

    fixing the problem with the function: try/catch addition to handle error, generating ids then referencing them, adding products inside the seed function

--- seed.js
+++ seed.js
@@ -22,36 +22,50 @@ const categories = [
 const kids = categories.find(c => c.name === 'kids');
 const women = categories.find(c => c.name === 'women'); 
 const men = categories.find(c => c.name === 'men');
-const products = [
-    {
-        name: 'Dino T-shirt',
-        price: 100,
-        stock: 52,
-        inStock: true,
-        category: kids._id
-    },
-    {
-        name: 'Thong Pants',
-        price: 240,
-        stock: 13,
-        inStock: true,
-        category: women._id
-    },
-    {
-        name: 'Teal Blazer',
-        price: 310,
-        stock: 19,
-        inStock: true,
-        category: men._id
-    }];
+
 const seed = async ()=>{
-    await connectDB();
-    await Product.deleteMany();
-    await Category.deleteMany();
-    await Category.insertMany(categories);
-    await Product.insertMany(products);
-    await mongoose.disconnect();
-    process.exit(0);
-}
+    
+    try {
+        await connectDB();
+        await Product.deleteMany({});
+        await Category.deleteMany({});
+        const insertCategories = await Category.insertMany(categories);
+
+        const kids = insertCategories.find(c => c.name === 'kids');
+        const women = insertCategories.find(c => c.name === 'women');
+        const men = insertCategories.find(c => c.name === 'men');
+
+        const products = [
+            {
+                name: 'Dino T-shirt',
+                price: 100,
+                stock: 52,
+                inStock: true,
+                category: kids[0]._id
+            },
+            {
+                name: 'Thong Pants',
+                price: 240,
+                stock: 13,
+                inStock: true,
+                category: women[0]._id
+            },
+            {
+                name: 'Teal Blazer',
+                price: 310,
+                stock: 19,
+                inStock: true,
+                category: men[0]._id
+            }];
+
+        await Product.insertMany(products);
+        await Category.insertMany(categories);
+        await mongoose.disconnect();
+        process.exit(0);
+    } catch (err) {
+        await mongoose.disconnect();
+        process.exit(1);
+    }
+};
 
 seed();
